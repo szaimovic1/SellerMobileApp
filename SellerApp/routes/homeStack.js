38f4742cd7d451cc75c049e27bc8fd57ec417c39
kamberovicmubina <mkamberovi1@etf.unsa.ba>
2020-03-25 14:03:10 +0100

    Added navigation

---
+++ SellerApp/routes/homeStack.js
@@ -0,0 +1,26 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import DisplayProducts from '../screens/displayProducts';
+import Header from '../components/header';
+import React from 'react';
+
+
+const screens = {
+    DisplayProducts: {
+        screen: DisplayProducts,
+        navigationOptions: ({ navigation }) => {
+            return {
+                headerTitle: () => <Header navigation={navigation} title='Products' />
+            }
+        }
+    },
+    /* Ovdje dodati screen za dodatne informacije o proizvodima, ali umjesto headerTitle treba imati prop title koji prima obicni string */
+};
+
+const HomeStack = createStackNavigator (screens, {
+    defaultNavigationOptions: {
+        headerStyle: { height: 80},
+   }
+});
+
+export default createAppContainer(HomeStack);
