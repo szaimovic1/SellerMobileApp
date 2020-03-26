f93c4cea3d1c0798ab6d58e6db23808b42a6f426
ldrkic1 <ldrkic1@etf.unsa.ba>
2020-03-26 21:16:30 +0100

    Style of products is changed..

--- SellerApp/screens/displayProducts.js
+++ SellerApp/screens/displayProducts.js
@@ -1,13 +1,7 @@
 import React, { useState }  from 'react';
-<<<<<<< HEAD
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
-import { Card, WingBlank, WhiteSpace } from '@ant-design/react-native';
-import CardBody from '@ant-design/react-native/lib/card/CardBody';
-=======
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image} from 'react-native';
 import { WingBlank, WhiteSpace } from '@ant-design/react-native';
 import { Card } from 'react-native-paper';
->>>>>>> 54195cc53efc667db63cbf48757ba655f2f62e82
 
 export default function DisplayProducts() {  
     /* Privremeni testni podaci */
@@ -19,49 +13,61 @@ export default function DisplayProducts() {
     { title: 'Topla čokolada', price: '4.00KM', quantitiy: 0, key: '6' }
   ]);
   const getStyle = (quantitiy) => {
-    if(quantitiy > 50) {
-      return {
-        borderRadius: 20,
-        shadowOffset:{  width: 10,  height: 10,  },
-        shadowColor: '#eee',
-        shadowOpacity: 1.0, 
-      }
-     }
-    else if(quantitiy>0 && quantitiy<=50) {
-      return {
-        backgroundColor: '#ffc2b3',
-        borderRadius: 20,
-        shadowOffset:{  width: 10,  height: 10,  },
-        shadowColor: '#ff9900',
-        //shadowColor: '#eee', //defaultna
-        //shadowColor: '#ffb380', //svjetlija nijansa
-        shadowOpacity: 1.0, 
-        //borderColor: '#ffb380'
+    if(quantitiy>0 && quantitiy<=50) {
+      return { //ukoliko bude potrebno i za kritične
+          height: 60,
+          backgroundColor: '#ffc2b3',
+          paddingVertical: 10,
+          paddingHorizontal: 10,
+          borderWidth: 0.2,
+          borderColor: '#d9d9d9',
+          borderRadius: 20,
        }
     }
     else {
       return {
-        backgroundColor: '#e6e6e6',
+        height: 60,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        borderWidth: 0.2,
+        borderColor: '#d9d9d9',
         borderRadius: 20,
-        shadowOffset:{  width: 10,  height: 10,  },
-        shadowColor: '#ff1a1a',
-        //shadowColor: '#eee', //defaultna
-        //shadowColor: '#ff9999' //svjetlija nijansa
-        shadowOpacity: 1.0, 
-        //borderColor: '#ff9999'
       }
     }
   }
-
-  const getTextStyle = (quantitiy) => {
+  const getTitleStyle = (quantitiy) => {
     if(quantitiy == 0) {
       return {
-        height: 50,
-        color: '#ffffff'
+        paddingTop: 10,
+        color: '#e6e6e6'
       }
     } else {
       return {
-        height: 50,
+        paddingTop: 10,
+        color: '#000000'
+      }
+    }
+  }
+  const getSubtitleStyle = (quantitiy) => {
+    if(quantitiy == 0) {
+      return {
+        paddingBottom: 10,
+        color: '#e6e6e6'
+      }
+    } else {
+      return {
+        paddingBottom: 10,
+        color: '#000000'
+      }
+    }
+  }
+  const getTextStyle = (quantitiy) => {
+    if(quantitiy == 0) {
+      return {
+        color: '#e6e6e6'
+      }
+    } else {
+      return {
         color: '#000000'
       }
     }
@@ -73,32 +79,17 @@ export default function DisplayProducts() {
         return (
           <TouchableOpacity>
           <WingBlank size="lg">
-<<<<<<< HEAD
-            <Card style={getStyle(item.quantitiy)}>
-              <Card.Header
-                title={ item.title }
-                //titleStyle = {{color:'white'}}
-                //titleStyle = {getTextStyle(item.quantitiy)}
-                thumbStyle={{ width: 30, height: 30 }}
-                thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
-                extra={ item.price }
-                style={{height: 50}}
-                //style={getTextStyle(item.quantitiy)}
-              />
-            </Card>
-=======
             <Card.Title
               title={item.title}
-              titleStyle={{color:'black', paddingTop: 10}}
+              titleStyle={getTitleStyle(item.quantitiy)}
               subtitle="Card Subtitle"
-              subtitleStyle={{color:'black', paddingBottom: 10}}
+              subtitleStyle={getSubtitleStyle(item.quantitiy)}
               left={(props) => <Image 
                 style={{width: 35, height: 35}}
                 source={{ uri: 'https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg',}}></Image>}
-              right={(props) => <Text>{item.price}</Text>}
+              right={(props) => <Text style={getTextStyle(item.quantitiy)}>{item.price}</Text>}
               style={styles.card}
             />
->>>>>>> 54195cc53efc667db63cbf48757ba655f2f62e82
           </WingBlank>
           <WhiteSpace size="lg" />
           </TouchableOpacity>
@@ -124,12 +115,6 @@ const styles = StyleSheet.create({
     borderWidth: 0.2,
     borderColor: '#d9d9d9',
     borderRadius: 20,
-<<<<<<< HEAD
-    shadowOffset:{  width: 10,  height: 10,  },
-    shadowColor: '#eee',
-    shadowOpacity: 1.0,  
-=======
->>>>>>> 54195cc53efc667db63cbf48757ba655f2f62e82
   },
   refreshBtn: {
     alignSelf: "center",
