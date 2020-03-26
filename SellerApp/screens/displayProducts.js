dd2a9c4012cb3948e22aa4bce896773cadf5bef2
ldrkic1 <ldrkic1@etf.unsa.ba>
2020-03-26 17:30:07 +0100

    Changed style of products

--- SellerApp/screens/displayProducts.js
+++ SellerApp/screens/displayProducts.js
@@ -1,6 +1,7 @@
 import React, { useState }  from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
 import { Card, WingBlank, WhiteSpace } from '@ant-design/react-native';
+import CardBody from '@ant-design/react-native/lib/card/CardBody';
 
 export default function DisplayProducts() {  
     /* Privremeni testni podaci */
@@ -22,20 +23,40 @@ export default function DisplayProducts() {
      }
     else if(quantitiy>0 && quantitiy<=50) {
       return {
+        backgroundColor: '#ffc2b3',
         borderRadius: 20,
         shadowOffset:{  width: 10,  height: 10,  },
         shadowColor: '#ff9900',
+        //shadowColor: '#eee', //defaultna
+        //shadowColor: '#ffb380', //svjetlija nijansa
         shadowOpacity: 1.0, 
-        //borderColor: '#ff9900',
+        //borderColor: '#ffb380'
        }
     }
     else {
       return {
+        backgroundColor: '#e6e6e6',
         borderRadius: 20,
         shadowOffset:{  width: 10,  height: 10,  },
         shadowColor: '#ff1a1a',
+        //shadowColor: '#eee', //defaultna
+        //shadowColor: '#ff9999' //svjetlija nijansa
         shadowOpacity: 1.0, 
-        //borderColor: '#ff1a1a'
+        //borderColor: '#ff9999'
+      }
+    }
+  }
+
+  const getTextStyle = (quantitiy) => {
+    if(quantitiy == 0) {
+      return {
+        height: 50,
+        color: '#ffffff'
+      }
+    } else {
+      return {
+        height: 50,
+        color: '#000000'
       }
     }
   }
@@ -49,10 +70,13 @@ export default function DisplayProducts() {
             <Card style={getStyle(item.quantitiy)}>
               <Card.Header
                 title={ item.title }
+                //titleStyle = {{color:'white'}}
+                //titleStyle = {getTextStyle(item.quantitiy)}
                 thumbStyle={{ width: 30, height: 30 }}
                 thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
                 extra={ item.price }
                 style={{height: 50}}
+                //style={getTextStyle(item.quantitiy)}
               />
             </Card>
           </WingBlank>
