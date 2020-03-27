5b10b84bb6a3d0dcf7caf55aa957fb1d913deecc
tproho1 <44259164+tproho1@users.noreply.github.com>
2020-03-27 17:09:36 +0100

    added login screen

--- SellerApp/screens/login.js
+++ SellerApp/screens/login.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { View } from "react-native";
+import React, { Component } from 'react'
+import { View, TextInput, Text, Button, Alert } from 'react-native'
+import styles from './loginStyles'
 
-export default function Login() {  
-    return (
-        <View>
+class Login extends Component {
 
-        </View>
-    )
+    state={username: "", password: ""}
+
+   
+    
+    checkLogin(){ 
+        const {username, password} = this.state
+        if(username == 'admin' && password == 'admin'){
+            console.warn('Login is correct')
+            this.props.navigation.navigate('displayProducts')
+        } else{
+            Alert.alert ('Error', 'Username/Password mismatch',[{
+                text: 'Okay'
+            }])
+        }
+    }
+
+    render(){
+      const { heading, input, parent } = styles
+
+        return (
+            <View style={parent}>
+                <Text style={heading}>Login into the application</Text>
+                <TextInput style={input} placeholder="Username" onChangeText={text => this.setState({username: text})}/>
+                <TextInput style={input} secureTextEntry={true} placeholder="Password" onChangeText={text => this.setState({password: text})} />
+
+
+                <Button title={"Login"} onPress={_ => this.checkLogin()} />
+            </View>
+        )
+    }
 }
+
+export default Login
