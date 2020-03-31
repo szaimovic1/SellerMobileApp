d0c5aecfcde275dc2272f2f5a2e3f8da5fdd9112
kamberovicmubina <mkamberovi1@etf.unsa.ba>
2020-03-31 11:40:53 +0200

    added keyboard dismiss on displayProducts

--- SellerApp/screens/displayProducts.js
+++ SellerApp/screens/displayProducts.js
@@ -1,11 +1,14 @@
 
 import React, { useState, useEffect }  from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image, ImageBackground, Dimensions, Modal, TouchableHighlight, RefreshControl, Platform} from 'react-native';
+import { Text, View, TouchableOpacity, ScrollView, Image, ImageBackground, 
+        Modal, TouchableHighlight, RefreshControl, 
+        Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { WingBlank, WhiteSpace } from '@ant-design/react-native';
 import { Card } from 'react-native-paper';
 import {AsyncStorage} from 'react-native';
-import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 import Filter from '../components/filter';
+import styles from '../styles/productStyles';
 
 
 export default function DisplayProducts() {
@@ -129,165 +132,77 @@ export default function DisplayProducts() {
     }
 
   return (
-    
-    <ImageBackground source={require('../images/background2.png')} 
-      style={[styles.container, 
-      modalVisible ? {opacity: 0.05} : '1']}>
-      <Modal
-        style={styles.centeredView}
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}>
-        <View style={styles.modal}>
-            <Image
-              style={styles.modalImage}
-              source={{ uri: modalData.image }}
-            />
-        </View>
-        <View style={styles.centeredView}>
-          <View style={{...styles.modalView, marginBottom: '50%'}}>
-              <Text style={styles.modalTitle}>{modalData.name}</Text>
-            <Text style={styles.modalText}>Price: <Text style={{fontWeight: "bold"}}>{modalData.price} KM</Text></Text>
-            <Text style={getStyle(modalData.quantity)}>Quantity: <Text style={{fontWeight: "bold"}}>{modalData.quantity} {modalData.unit}</Text><Text> {isProductQuantitySmall(modalData.quantity)}</Text></Text>
-            <Text style={styles.modalText}>Discount: <Text style={{fontWeight: "bold"}}>{modalData.discount} %</Text></Text>
-            <TouchableHighlight
-              style={{ ...styles.openButton, backgroundColor: 'rgba(0,0,55,50)' }}
-              onPress={() => {
-                setModalVisible(false);
-              }}
-            >
-              <Text style={styles.textStyle}>&#215;</Text>
-            </TouchableHighlight>
+    <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss();}}>
+      <ImageBackground source={require('../images/background2.png')} 
+        style={[styles.container, 
+        modalVisible ? {opacity: 0.05} : '1']}>
+        <Modal
+          style={styles.centeredView}
+          animationType="fade"
+          transparent={true}
+          visible={modalVisible}>
+          <View style={styles.modal}>
+              <Image
+                style={styles.modalImage}
+                source={{ uri: modalData.image }}
+              />
           </View>
-        </View>
-      </Modal>
+          <View style={styles.centeredView}>
+            <View style={{...styles.modalView, marginBottom: '50%'}}>
+                <Text style={styles.modalTitle}>{modalData.name}</Text>
+              <Text style={styles.modalText}>Price: <Text style={{fontWeight: "bold"}}>{modalData.price} KM</Text></Text>
+              <Text style={getStyle(modalData.quantity)}>Quantity: <Text style={{fontWeight: "bold"}}>{modalData.quantity} {modalData.unit}</Text><Text> {isProductQuantitySmall(modalData.quantity)}</Text></Text>
+              <Text style={styles.modalText}>Discount: <Text style={{fontWeight: "bold"}}>{modalData.discount} %</Text></Text>
+              <TouchableHighlight
+                style={{ ...styles.openButton, backgroundColor: 'rgba(0,0,55,50)' }}
+                onPress={() => {
+                  setModalVisible(false);
+                }}
+              >
+                <Text style={styles.textStyle}>&#215;</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
 
-      <Filter updateList={updateList} />
-      <ScrollView
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={getProducts} />
-        }>
-        {products.map((item) => {
-          return (
-            <TouchableOpacity key={item.id}
-                onLongPress={ () => {
-                ModalFetcher(item.id); 
-                setModalVisible(true);}}>
-              <WingBlank size="lg">
-                <Card.Title            
-                  title={item.name}
-                  titleStyle={getTitleStyle(item.quantity)}
-                  subtitleStyle={getSubtitleStyle(item.quantity)}
+        <Filter updateList={updateList} />
+        <ScrollView
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={getProducts} />
+          }>
+          {products.map((item) => {
+            return (
+              <TouchableOpacity key={item.id}
+                  onLongPress={ () => {
+                  ModalFetcher(item.id); 
+                  setModalVisible(true);}}>
+                <WingBlank size="lg">
+                  <Card.Title            
+                    title={item.name}
+                    titleStyle={getTitleStyle(item.quantity)}
+                    subtitleStyle={getSubtitleStyle(item.quantity)}
 
-                  left={(props) => {
-                    const img = item.imageBase64;
-                    return <Image 
-                      style={[{width: 35, height: 35}, modalVisible ? {opacity: 0} : '1']}
-                      source={{ uri: img }} /> 
-                  }}
-                  right={(props) => <Text style={[getTextStyle(item.quantity),
-                     modalVisible ? {color: 'rgba(0,0,0,0.7)'} : '']}>{item.price} KM</Text>}
-                  style={[styles.card, 
-                    modalVisible ? {backgroundColor: 'rgba(0,0,0,0.7)'} : '', 
-                    modalVisible ? {borderColor: 'rgba(0,0,0,0.7)'} : '']}
-                />
-              </WingBlank>
-              <WhiteSpace size="lg" />
-              </TouchableOpacity>
+                    left={(props) => {
+                      const img = item.imageBase64;
+                      return <Image 
+                        style={[{width: 35, height: 35}, modalVisible ? {opacity: 0} : '1']}
+                        source={{ uri: img }} /> 
+                    }}
+                    right={(props) => <Text style={[getTextStyle(item.quantity),
+                      modalVisible ? {color: 'rgba(0,0,0,0.7)'} : '']}>{item.price} KM</Text>}
+                    style={[styles.card, 
+                      modalVisible ? {backgroundColor: 'rgba(0,0,0,0.7)'} : '', 
+                      modalVisible ? {borderColor: 'rgba(0,0,0,0.7)'} : '']}
+                  />
+                </WingBlank>
+                <WhiteSpace size="lg" />
+                </TouchableOpacity>
 
-          )
-        }
-        )}
-      </ScrollView>
-    </ImageBackground>
-
+            )
+          }
+          )}
+        </ScrollView>
+      </ImageBackground>
+    </TouchableWithoutFeedback>    
   )             
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 80,
-  },
-  card: {
-    height: 60,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    borderWidth: 0.2,
-    borderColor: '#d9d9d9',
-    borderRadius: 20,
-    backgroundColor:'white'
-  },
-  refreshBtn: {
-    alignSelf: "center",
-    marginTop: 20,
-    marginBottom: 30,
-    width: 160,
-  },
-
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
-  },
-  modalView: {
-    ...Platform.select({
-      ios: {
-        height: 350,
-      },
-    }),
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "rgba(0,0,55,50)",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  openButton: {
-    backgroundColor: "#F194FF",
-    justifyContent: "center",
-    height: 40,
-    width: 40,  
-    borderRadius:400
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center", 
-    fontSize: 30
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-    fontSize: 20
-  },
-  modalTitle: {
-    color: "white", 
-    backgroundColor: 'rgba(0,0,55,50)', 
-    fontWeight: "bold", 
-    fontSize: 30, 
-    marginBottom: 30, 
-    padding: 10,
-    textAlign: "center", 
-    width: 250
-  },
-  modalImage: { width: Dimensions.get('window').width, 
-  height: '55%', 
-  resizeMode: 'stretch', 
-  marginTop: '20%',
-  },
-  rightIcon : {
-    position: 'absolute',
-    left: 20,
-    marginTop: 20,
-    marginBottom: 30,
-  },
-});
