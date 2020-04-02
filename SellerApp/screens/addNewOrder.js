24590a47dfb449aba15bc0cf877b8b7b47f670b0
ldrkic1 <ldrkic1@etf.unsa.ba>
2020-04-02 15:10:58 +0200

    added input for note

--- SellerApp/screens/addNewOrder.js
+++ SellerApp/screens/addNewOrder.js
@@ -1,15 +1,38 @@
 import React from 'react'
-import { View, Text, ScrollView, Image, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, Image, ImageBackground, TouchableOpacity, TextInput } from 'react-native';
 import { WingBlank, WhiteSpace } from '@ant-design/react-native';
 import styles from '../styles/productStyles';
 import { Card } from 'react-native-paper';
 import { saveNewOrder } from '../functions/storage';
 
 export default function AddNewOrder ({ navigation }) {
+    const param = navigation.getParam('data');
+    const order = param.newOrder; // objekat koji sadrzi kao atribute: products-niz proizvoda,tableNr i served
+    const getTableNumber = (note) => {
+        var words = note.split('');
+        words.forEach(word => {
+            if(!isNaN(word)) {
+                //console.log('ima broj stola ' + word);
+                return Number(word);
+            }
+        });
+        return 0;
+    }
+    const checkTableNumberAndServed = (note) => {
+        note = note.toLowerCase();
+        //trazimo rijec koja naznacava da je narudzba usluzena
+        if(note.includes('served') || note.includes('usluzena') || note.includes('uslužena') || note.includes(usluzeno) || note.includes(usluženo)) {
+            order.served = true;
+        }
+        else {
+            order.served = false;
+        }
+
+        note.tableNr = getTableNumber(note);
+    } 
     return (
         <ImageBackground source={require('../images/background2.png')} 
         style={styles.container}>
-
             <View style={styles.btnContainer}>
                 <View style={{flex: 1}}>
                 <TouchableOpacity
@@ -31,7 +54,7 @@ export default function AddNewOrder ({ navigation }) {
                     <Text style={styles.sumbitText}>Cancel</Text>
                 </TouchableOpacity></View>
             </View>
-
+            <TextInput multiline placeholder='Note' style={styles.input} onChangeText={text => checkTableNumberAndServed(text)}/>
             <ScrollView>
                 {navigation.state.params.data.newOrder.products.map(( item ) => {
                     return (
