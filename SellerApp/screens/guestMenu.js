ce1fba0646484d94ca019cbd3a8155b5d4ed90be
kamberovicmubina <mkamberovi1@etf.unsa.ba>
2020-04-09 12:56:28 +0200

    ubaceno dobavljanje opisa proizvoda

--- SellerApp/screens/guestMenu.js
+++ SellerApp/screens/guestMenu.js
@@ -113,7 +113,7 @@ export default function GuestMenu({ navigation }) {
                         </View>      
                         <ScrollView>
                         <Text style={{...styles.smallerText, color: '#404040'}}>Ingredients:
-                        <Text style={styles.smallerText}> sugar, coffein, sugar, sugar, sugar, sugar,fds,fds,fmfrjfdjfjijfkdjsmkfsdlmfsd,fsdkfdlslfksdkfjdsfjdsfjidsjifsdfkdskdsjjdskdskadkfjsjfsdjkdsfjdsjjfdkfsdjfdsjij</Text>
+                        <Text style={styles.smallerText}>{item.ingredients}</Text>
                         </Text></ScrollView>
                     </View>
                     
