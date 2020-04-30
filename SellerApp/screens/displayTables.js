e74d2e79357fa54803398c3bf4046a9c5f223735
ldrkic1 <ldrkic1@etf.unsa.ba>
2020-04-30 13:12:52 +0200

    Graficki prikaz stolova

--- SellerApp/screens/displayTables.js
+++ SellerApp/screens/displayTables.js
@@ -1,39 +1,39 @@
 import React, { useState } from 'react';
 import { View, Text, Image, ImageBackground,TouchableOpacity, FlatList} from 'react-native';
+import styles from '../styles/tablesStyles';
 
 export default function DisplayTables({ navigation }) {
     const test= [
-        {table: 1, x: 1, y: 1},
-        {table: 2, x: 1, y: 2},
-        {table: 3, x: 1, y: 3},
-        {table: 6, x: 2, y: 1},
-        {table: 7, x: 2, y: 3},
-        {table: 9, x: 4, y: 1},
-        {table: 10, x: 4, y: 3},
-        {table: 11, x: 5, y: 1},
-        {table: 12, x: 5, y: 2},
-        {table: 13, x: 5, y: 3}
+        {table: 1},
+        {table: 2},
+        {table: 3},
+        {table: 4},
+        {table: 5},
+        {table: 6},
+        {table: 7},
+        {table: 8},
+        {table: 9},
+        {table: 10},
+        {table: 11},
+        {table: 12},
+        {table: 13},
+        {table: 14}
     ];
-    const numberOfRows = (tables) => {
-        var max = 0;
-        //console.log(tables.length);
-        for(var i = 0; i < tables.length; i++)  {
-            if(max<tables[i].x) max = tables[i].x;
+    
+    const makeTableMap = (tables, columnNumber) => {
+        var tableMap = tables;
+        const numberOfFullRows = Math.floor(tables.length / columnNumber);
+        let numberOfElementsInLastRow = tables.length - (numberOfFullRows*columnNumber);
+        const empty = {
+            table: 0,
+            empty:true
+        };
+        while(numberOfElementsInLastRow != columnNumber && numberOfElementsInLastRow != 0) {
+            tableMap.push(empty);
+            numberOfElementsInLastRow = numberOfElementsInLastRow + 1;
         }
-        //console.log(max);
-        return max;
-    }
-    const numberOfColumns = (tables) => {
-        var max = 0;
-        //console.log(tables.length);
-        for(var i = 0; i < tables.length; i++)  {
-            if(max<tables[i].y) max = tables[i].y;
-        }
-        //console.log(max);
-        return max;
-    }
-    const makeTableMap = (tables) => {
-        tables.sort(function(t1, t2) {
+        //u slucaju potrebe za posebnim rasporedom stolova
+        /*tables.sort(function(t1, t2) {
             return t1.y - t2.y;
         });
         tables.sort(function(t1, t2) {
@@ -63,37 +63,35 @@ export default function DisplayTables({ navigation }) {
                     tableMap.push(containTableOnIJ[0]);
                 }
             }
-        }
-        
-        console.log('ovoliko fali '+borja);
+        }*/
         return tableMap;
     }
-    const tables = makeTableMap(test);
+    const columnNumber = 3;
+    const tables = makeTableMap(test, columnNumber);
     return (
-        <ImageBackground style={{ flex: 1,
-            paddingTop: 30}} source={require('../images/background2.png')}>
-            <View style={{backgroundColor: '#05132e', alignContent: 'center', padding:10}}>
+        <ImageBackground style={styles.imageBackground} source={require('../images/background2.png')}>
+            <View style={styles.tablesView}>
             <FlatList 
                 data={tables}
                 renderItem={({item}) => {
                     if(item.empty === true) {
                         return(
-                            <View style={{borderRadius:4,backgroundColor:'transparent',flex: 1, flexDirection: 'column', margin: 2, alignItems:'center' }}>
-                                <Image style={{ width: 50, height: 50}}/>
+                            <View style={styles.invisible}>
+                                <Image style={styles.imageTable}/>
                                 <Text></Text>
                             </View>
                         );
                     }
                     else {
                         return(  
-                            <TouchableOpacity style={{borderRadius:4, backgroundColor: 'white', flex: 1, flexDirection: 'column', margin: 2, alignItems:'center' }}>
-                                <Image style={{ width: 50, height: 50}} source={require("../images/circle.png")}/>
-                                <Text style={{color:'#05132e', fontWeight:'bold'}}>Table {item.table}</Text>
+                            <TouchableOpacity style={styles.tableStyle}>
+                                <Image style={styles.imageTable} source={require("../images/circle.png")}/>
+                                <Text style={styles.text}>Table {item.table}</Text>
                             </TouchableOpacity> 
                         );  
                     }
                 }}
-                numColumns={numberOfColumns(test)}
+                numColumns={columnNumber}
                 keyExtractor={(item) => item.table.toString()}
             />
             </View>
