019889e147ec758d4551dab2dadc20d247abc4db
tproho1 <44259164+tproho1@users.noreply.github.com>
2020-05-03 23:26:50 +0200

    added password change for user that is loged in

--- SellerApp/screens/login.js
+++ SellerApp/screens/login.js
@@ -98,7 +98,7 @@ export default function Login({ navigation }) {
 
                 setLastNotificationID();
                 registerForPushNotifications();
-
+                setItemStorage('password', password);
                 navigation.navigate('DisplayProducts')
             }
             else {
