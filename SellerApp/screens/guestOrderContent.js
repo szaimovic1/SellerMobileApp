26e183b5a2da3834a4a11020e28d92458bdd9292
kamberovicmubina <mkamberovi1@etf.unsa.ba>
2020-05-08 00:51:54 +0200

    popravljen edit i brisanje u narudzbama

--- SellerApp/screens/guestOrderContent.js
+++ SellerApp/screens/guestOrderContent.js
@@ -51,6 +51,17 @@ export default function GuestOrderContent({ navigation }) {
   }, []);
 
   useEffect(() => {
+    if(products.length === 0 && !navigation.state.params.data.item.served) {
+      console.log("NEma proizvdoa");
+      deleteGuestOrder(navigation.state.params.data.item.id);
+      Alert.alert('Success', 'Order deleted successfully!', [{
+        text: 'Okay'
+      }])
+      navigation.navigate('DisplayOrders');
+    }
+  }, [products])
+
+  useEffect(() => {
     calculateTotalPrice(receiptItems);
   }, [price]);
 
@@ -59,16 +70,22 @@ export default function GuestOrderContent({ navigation }) {
   }
 
   const editButton = () => {
-    setEditButtonVisible(!editButtonVisible);
-    setEditInputVisible(!editInputVisible);
-    setAddButtonDisabled(!addButtonDisabled);
+    if(!navigation.state.params.data.item.served) {
+      setEditButtonVisible(!editButtonVisible);
+      setEditInputVisible(!editInputVisible);
+      setAddButtonDisabled(!addButtonDisabled);
 
-    let newProductsQuantity = [];
-    products.map(product => {
-      newProductsQuantity.push({ id: product.id, quantity: product.times });
-    });
+      let newProductsQuantity = [];
+      products.map(product => {
+        newProductsQuantity.push({ id: product.id, quantity: product.times });
+      });
 
-    setPrevProductsQuantity(newProductsQuantity);
+      setPrevProductsQuantity(newProductsQuantity);
+    } else {
+      Alert.alert('Error', 'You cannot edit a served order!', [{
+        text: 'Okay'
+      }])
+    }
   }
 
   const checkButton = () => {
